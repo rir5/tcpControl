6225431c6ef3c05f91e186739a16d95352b42faa
rir5 <rafy.reyes.007@gmail.com>
2014-01-17 03:46:38 -0500

    Arduino

---
+++ Arduino/Motor_shield_demo_code3.ino
@@ -0,0 +1,98 @@
+//. Motor driver shield- 2012 Copyright (c) Seeed Technology Inc.
+// 
+//  Original Author: Jimbo.we
+//  Contribution: LG
+//  
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+int pinI1=8;//define I1 interface
+int pinI2=11;//define I2 interface 
+int speedpinA=9;//enable motor A
+int pinI3=12;//define I3 interface 
+int pinI4=13;//define I4 interface 
+int speedpinB=10;//enable motor B
+int spead =255;//define the spead of motor 255 is max 0 is lowest
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pinI1,OUTPUT);
+  pinMode(pinI2,OUTPUT);
+  pinMode(speedpinA,OUTPUT);
+  pinMode(pinI3,OUTPUT);
+  pinMode(pinI4,OUTPUT);
+  pinMode(speedpinB,OUTPUT);
+}
+ 
+void forward()
+{
+     analogWrite(speedpinB,spead);
+     digitalWrite(pinI4,HIGH);//turn DC Motor B move clockwise
+     digitalWrite(pinI3,LOW);
+}
+void backward()//
+{
+     analogWrite(speedpinB,spead);
+     digitalWrite(pinI4,LOW);//turn DC Motor B move anticlockwise
+     digitalWrite(pinI3,HIGH);
+}
+void left()//
+{
+     analogWrite(speedpinA,spead);//input a simulation value to set the speed
+     digitalWrite(pinI2,HIGH);//turn DC Motor A move clockwise
+     digitalWrite(pinI1,LOW);
+}
+void right()//
+{
+     analogWrite(speedpinA,spead);//input a simulation value to set the speed
+     digitalWrite(pinI2,LOW);//turn DC Motor A move clockwise
+     digitalWrite(pinI1,HIGH);
+}
+void stop()//
+{
+     digitalWrite(speedpinA,LOW);// Unenble the pin, to stop the motor. this should be done to avid damaging the motor. 
+     digitalWrite(speedpinB,LOW);
+ 
+}
+
+void loop()
+{
+  byte val;
+  if(Serial.available()){
+    val = Serial.read();
+    Serial.write(val);
+    if(val == '0'){
+      stop();
+    }
+    if(val == '1'){
+      forward();
+    }
+    if(val == '2'){
+      backward();
+    }
+    if(val == '3'){
+      left();
+    }
+    if(val =='4'){
+      right();
+    }
+    if(val == '5'){
+      spead = spead + 10;
+    }
+    if(val == '6'){
+      spead = spead - 10;
+    }
+  }
+}
